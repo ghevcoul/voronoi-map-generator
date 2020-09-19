48d9d8b0f776668ab188356d52b0416b3e687e1a
Gavin Heverly-Coulson <gavin@quantumgeranium.com>
2020-09-19 12:44:33 -0700

    setup basic project structure and build

---
+++ build.sbt
@@ -0,0 +1,6 @@
+
+organization := "com.quantumgeranium"
+name := "voronoi-mapper"
+version := "1.0.0-SNAPSHOT"
+
+scalaVersion := "2.13.3"
---
+++ src/main/scala/com/quantumgeranium/voronoi-mapper/VoronoiMapper.scala
@@ -0,0 +1,9 @@
+package com.quantumgeranium.voronoi-mapper
+
+object VoronoiMapper {
+
+  def main(args: Array[String]): Unit = {
+    println("Hello")
+  }
+
+}
