0cfbc3dbabab0bdda7a010d21b800e9cc35fc8e7
Gavin Heverly-Coulson <gavin@quantumgeranium.com>
2021-09-06 14:18:32 -0700

    extend main function

--- src/main/scala/com/quantumgeranium/voronoi_mapper/VoronoiMapper.scala
+++ src/main/scala/com/quantumgeranium/voronoi_mapper/VoronoiMapper.scala
@@ -1,7 +1,7 @@
 package com.quantumgeranium.voronoi_mapper
 
 import com.quantumgeranium.voronoi_mapper.triangulation.DelaunayTriangulation
-import com.quantumgeranium.voronoi_mapper.geom.Point
+import com.quantumgeranium.voronoi_mapper.geom.{BoundingBox, Point}
 
 import scala.collection.mutable.ArrayBuffer
 import scala.util.Random
@@ -10,22 +10,10 @@ object VoronoiMapper {
 
   val xDimension = 1000
   val yDimension = 1000
+  val boundingBox: BoundingBox = BoundingBox(0, xDimension, 0, yDimension)
   val numPoints = 250
 
   val random = new Random(95709)
-
-  def initializeCenters(): ArrayBuffer[Point] = {
-    val writer = new ImageWriter(xDimension, yDimension)
-    writer.setColor("black")
-    val points = new ArrayBuffer[Point]()
-    for (_ <- 0 until numPoints) {
-      val p = new Point(random.nextDouble() * xDimension, random.nextDouble() * yDimension)
-      writer.drawPoint(p)
-      points.addOne(p)
-    }
-    writer.writeImage("center_points.png")
-    points
-  }
 
   def main(args: Array[String]): Unit = {
 
@@ -38,7 +26,21 @@ object VoronoiMapper {
     delaunay.drawTriangulation("delaunay_triangulation.png")
 
     val graph = delaunay.convertToDualGraph()
+    graph.computeGraphProperties(boundingBox)
     graph.drawGraph(xDimension, yDimension)
+  }
+
+  def initializeCenters(): ArrayBuffer[Point] = {
+    val writer = new ImageWriter(xDimension, yDimension)
+    writer.setColor("black")
+    val points = new ArrayBuffer[Point]()
+    for (_ <- 0 until numPoints) {
+      val p = new Point(random.nextDouble() * xDimension, random.nextDouble() * yDimension)
+      writer.drawPoint(p)
+      points.addOne(p)
+    }
+    writer.writeImage("center_points.png")
+    points
   }
 
 }
